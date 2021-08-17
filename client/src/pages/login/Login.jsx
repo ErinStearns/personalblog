8d8d7ccb83192713e94cab727bd96e30c7332ed6
Erin Stearns <erinstearns822@icloud.com>
2021-08-17 12:18:07 -0400

    test

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -8,7 +8,7 @@ export default function Login() {
             <form className="loginForm">
                 <label>Email</label>
                 <input type="text" className="loginInput" placeholder="Enter your email..." />
-                <label>Password</label>
+                <label>Pssword</label>
                 <input type="password" className="loginInput" placeholder="Enter your password..." />
                 <button className="loginButton">Login</button>
             </form>
