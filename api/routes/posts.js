476cc60c65d7b9ca46302c9d38484bdca5165f2d
Erin Stearns <erinstearns822@icloud.com>
2021-08-18 12:49:38 -0400

    fixed server issue

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 const Post = require('../models/Post');
 
 //CREATE POST
-router.post('/:id', async (req, res) => {
+router.post('/', async (req, res) => {
   const newPost = new Post(req.body);
   try {
     const savedPost = await newPost.save();
@@ -69,25 +69,25 @@ router.get('/:id', async (req, res) => {
 
 //GET ALL POSTS
 router.get('/', async (req, res) => {
-    const username = req.query.user;
-    const catname = req.query.cat;
-
-    try {
-        let posts;
-      if(username){
-          posts = await Post.find({username});
-      } else if(catName){
-          posts = await Post.find({categories:{
-              $in:[catName]
-          }})
-      } else{
-          posts = await Post.find();
-      }
-      res.status(200).json(posts)
-    } catch (err) {
-      res.status(500).json(err);
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
     }
-  });
-
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react';
 import Header from '../../components/header/Header';
 import Posts from '../../components/posts/Posts';
 import SideBar from '../../components/sidebar/SideBar';
 import './home.css';
+import axios from "axios";
 
 export default function Home() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(()=>{
+        const fetchPosts = async () => {
+            const res = await axios.get("/posts")
+            console.log(res)
+        }
+        fetchPosts();
+    },[])
+
     return (
         <>
         <Header/>
