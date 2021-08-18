eb140659b44f7ee1d6191b88cb3715e1cbff417a
Erin Stearns <erinstearns822@icloud.com>
2021-08-18 17:09:17 -0400

    finished registration, starting context api

--- client/src/components/sidebar/SideBar.jsx
+++ client/src/components/sidebar/SideBar.jsx
@@ -1,33 +1,50 @@
+import { useEffect, useState } from 'react';
 import './sidebar.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function SideBar() {
-    return (
-        <div className="sidebar">
-            <div className="sidebarItem">
-                <span className="sidebarTitle">ABOUT ME</span>
-                <img src="https://images.pexels.com/photos/725255/pexels-photo-725255.jpeg?auto=compress&cs=tinysrgb&h=150&w=252" alt="" />
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint aspernatur, voluptates.</p>
-            </div>
-            <div className="sidebarItem">
-                <span className="sidebarTitle">CATEGORIES</span>
-                <ul className="sidebarList">
-                    <li className="sidebarListItem">Life</li>
-                    <li className="sidebarListItem">Music</li>
-                    <li className="sidebarListItem">Style</li>
-                    <li className="sidebarListItem">Sport</li>
-                    <li className="sidebarListItem">Tech</li>
-                    <li className="sidebarListItem">Cinema</li>
-                </ul>
-            </div>
-            <div className="sidebarItem">
-                <span className="sidebarTitle">FOLLOW US</span>
-                <div className="sidebarSocial">
-                    <i className="sidebarIcon fab fa-facebook-square"></i>
-                    <i className="sidebarIcon fab fa-twitter-square"></i>
-                    <i className="sidebarIcon fab fa-pinterest-square"></i>
-                    <i className="sidebarIcon fab fa-instagram-square"></i>
-                </div>
-            </div>
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get('/categories');
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+  return (
+    <div className='sidebar'>
+      <div className='sidebarItem'>
+        <span className='sidebarTitle'>ABOUT ME</span>
+        <img
+          src='https://images.pexels.com/photos/725255/pexels-photo-725255.jpeg?auto=compress&cs=tinysrgb&h=150&w=252'
+          alt=''
+        />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint
+          aspernatur, voluptates.
+        </p>
+      </div>
+      <div className='sidebarItem'>
+        <span className='sidebarTitle'>CATEGORIES</span>
+        <ul className='sidebarList'>
+          {cats.map((c) => (
+            <Link to={`/?cat=${c.name}`} className='link'>
+              <li className='sidebarListItem'>{c.name}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className='sidebarItem'>
+        <span className='sidebarTitle'>FOLLOW US</span>
+        <div className='sidebarSocial'>
+          <i className='sidebarIcon fab fa-facebook-square'></i>
+          <i className='sidebarIcon fab fa-twitter-square'></i>
+          <i className='sidebarIcon fab fa-pinterest-square'></i>
+          <i className='sidebarIcon fab fa-instagram-square'></i>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,26 +3,28 @@ import Header from '../../components/header/Header';
 import Posts from '../../components/posts/Posts';
 import SideBar from '../../components/sidebar/SideBar';
 import './home.css';
-import axios from "axios";
+import axios from 'axios';
+import { useLocation } from 'react-router-dom';
 
 export default function Home() {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
 
-    useEffect(()=>{
-        const fetchPosts = async () => {
-            const res = await axios.get("/posts")
-            setPosts(res.data)
-        }
-        fetchPosts();
-    },[])
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get('/posts' + search);
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, [search]);
 
-    return (
-        <>
-        <Header/>
-        <div className="home">
-            <Posts posts={posts}/>
-            <SideBar />
-        </div>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <div className='home'>
+        <Posts posts={posts} />
+        <SideBar />
+      </div>
+    </>
+  );
 }
