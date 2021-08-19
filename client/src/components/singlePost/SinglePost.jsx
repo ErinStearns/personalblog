8b3960d63f01576e3016046855994622a465187e
Erin Stearns <erinstearns822@icloud.com>
2021-08-19 17:06:21 -0400

    delete posts

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,13 +1,16 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './singlepost.css';
+import { Context } from '../../context/Context';
 
 export default function SinglePost() {
   const location = useLocation();
   const path = location.pathname.split('/')[2];
   const [post, setPost] = useState({});
+  const PF = 'http://localhost:5000/images/';
+  const { user } = useContext(Context);
 
   useEffect(() => {
     const getPost = async () => {
@@ -17,36 +20,46 @@ export default function SinglePost() {
     getPost();
   }, [path]);
 
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${post._id}`, {
+      data: {username:user.username},
+    });
+      window.location.replace('/');
+
+    } catch (err) {}
+  };
+
   return (
     <div className='singlePost'>
       <div className='singlePostWrapper'>
         {post.photo && (
-          <img
-            src={post.photo}
-            alt=''
-            className='singlePostImg'
-          />
+          <img src={PF + post.photo} alt='' className='singlePostImg' />
         )}
         <h1 className='singlePostTitle'>
           {post.title}
-          <div className='singlePostEdit'>
-            <i className='singlePostIcon far fa-edit'></i>
-            <i className='singlePostIcon far fa-trash-alt'></i>
-          </div>
+          {post.username === user?.username && (
+            <div className='singlePostEdit'>
+              <i className='singlePostIcon far fa-edit'></i>
+              <i
+                className='singlePostIcon far fa-trash-alt'
+                onClick={handleDelete}
+              ></i>
+            </div>
+          )}
         </h1>
         <div className='singlePostInfo'>
           <span className='singlePostAuthor'>
-            Author: 
-            <Link to={`/?user=${post.username}`} className="link">
-            <b>{post.username}</b>
-            
+            Author:
+            <Link to={`/?user=${post.username}`} className='link'>
+              <b>{post.username}</b>
             </Link>
           </span>
-          <span className='singlePostDate'>{new Date(post.createdAt).toDateString()}</span>
+          <span className='singlePostDate'>
+            {new Date(post.createdAt).toDateString()}
+          </span>
         </div>
-        <p className='singlePostDesc'>
-          {post.desc}
-        </p>
+        <p className='singlePostDesc'>{post.desc}</p>
       </div>
     </div>
   );
